0cee1746a16c07b46ed79db9cd48e74168a1f5cc
Logan Woolf <loganwoolf@gmail.com>
2022-01-21 23:17:05 +0000

    Create Account class and Transaction class

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   }
 
+}
+
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+}
+
+class Deposit extends Transaction {
+
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.amount;
+  }
+}
+
+class Withdrawal extends Transaction {
+
+  commit() {
+    this.account.balance -= this.amount;
   }
 
 }
 
 
-
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+console.log('Balance:', myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
+console.log('Balance:', myAccount.balance);
 
-console.log('Balance:', balance);
+const t3 = new Deposit(120, myAccount);
+t3.commit();
+console.log('t3:', t3);
+console.log('Balance:', myAccount.balance);
+
